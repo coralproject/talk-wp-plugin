fe5f129741c3b93f04584c4c62b755046e81423a
Josh Kadis <josh@alleyinteractive.com>
2017-07-06 10:16:33 -0400

    add a helper function to determine if plugin is ready for use

--- talk.php
+++ talk.php
@@ -23,21 +23,39 @@ class Talk_Plugin {
 	public function __construct() {
 		require_once( CORAL_PROJECT_TALK_DIR . '/inc/class-talk-settings-page.php' );
 		add_filter( 'comments_template', function( $default_template_path ) {
-			$talk_url = get_option( 'coral_talk_base_url' );
-			if ( empty( $talk_url ) ) {
-				return $default_template_path;
-			}
-			return coral_talk_comments_template_path();
+			return coral_talk_plugin_is_usable() ?
+				coral_talk_get_comments_template_path() :
+				$default_template_path;
 		}, 99 );
 	}
 }
 
-function coral_talk_comments_template_path() {
+/**
+ * Assuming that the plugin is active (otherwise this function won't be available)
+ * determine if the required Talk instance URL option is set
+ *
+ * @return bool
+ */
+function coral_talk_plugin_is_usable() {
+	$talk_url = get_option( 'coral_talk_base_url' );
+	return ! empty( $talk_url );
+}
+
+/**
+ * Get absolute path to comments template file
+ *
+ * @return string File path
+ */
+function coral_talk_get_comments_template_path() {
 	return ( CORAL_PROJECT_TALK_DIR . '/inc/comments-template.php' );
 }
 
+/**
+ * Template tag to render the Coral Talk template without the performance hit of
+ * filtering comments_template()
+ */
 function coral_talk_comments_template() {
-	require( coral_talk_comments_template_path() );
+	require( coral_talk_get_comments_template_path() );
 }
 
 new Talk_Plugin;
